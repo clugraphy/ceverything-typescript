955bdbd87fa26242a9b8ad9127a1a01052781ba8
Catalin Stroe <alexandru.catalin.stroe@gmail.com>
2024-03-07 19:17:51 +0200

    not so much

--- app/(Home)/page.tsx
+++ app/(Home)/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import * as React from "react"
-import { useTheme } from "next-themes"
+import * as React from "react";
+import { useTheme } from "next-themes";
 import NavBar from "./components/NavBar";
 import HeroSection from "./components/HeroSection";
 import { Spotlight } from "@/components/ui/spotlight";
@@ -8,26 +8,24 @@ import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
 
-
+//TODO: Fix page bottom space. I think is from the footer.
 
 export default function Home() {
   return (
-
     <div className="min-h-screen bg-black overflow-hidden">
-      <div className=" dark:bg-black bg-white  dark:bg-grid-green-500/[0.1] bg-grid-red/[0.2]">
-
+      <div className=" dark:bg-black bg-white dark:bg-grid-green-500/[0.1] bg-grid-red/[0.2]">
         <div className="max-w-7xl mx-auto p-5">
           <NavBar />
           <HeroSection />
-
         </div>
       </div>
       <div className="max-w-7xl mx-auto p-5 mt-20">
         <Skills />
         <Projects />
-        <Spotlight />
+
+        {/* <Spotlight /> */}
         <Footer />
       </div>
-    </div >
+    </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,26 +2,32 @@ import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-
-import * as React from "react"
-import { Moon, Sun } from "lucide-react"
-import { useTheme } from "next-themes"
-
-import { Button } from "@/components/ui/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-
+import * as React from "react";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+// import { Button } from "@/components/ui/button";
+// import {
+//   DropdownMenu,
+//   DropdownMenuContent,
+//   DropdownMenuItem,
+//   DropdownMenuTrigger,
+// } from "@/components/ui/dropdown-menu"
 
 const spaceGrotesk = Space_Grotesk({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "C Everything Studio",
-  description: "C Everything Studio is dabbling in code and creating things. Made by Catalin Stroe nicknamed cLu.",
-  keywords: ["web3", "coding", "programming", "developer", "portfolio", "react", "nextjs"],
+  description:
+    "C Everything Studio is dabbling in code and creating things. Made by Catalin Stroe nicknamed cLu.",
+  keywords: [
+    "web3",
+    "coding",
+    "programming",
+    "developer",
+    "portfolio",
+    "react",
+    "nextjs",
+  ],
   metadataBase: new URL("http://localhost:3000"),
   authors: { name: "cLu from cEverything Studio" },
   openGraph: {
@@ -30,8 +36,7 @@ export const metadata: Metadata = {
     url: "https://ceverything.ro",
     siteName: "cEverything Studio",
     images: "/images/water7.png",
-
-  }
+  },
 };
 
 export default function RootLayout({
@@ -43,7 +48,12 @@ export default function RootLayout({
     <>
       <html lang="en">
         <body className={spaceGrotesk.className}>
-          <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
             {children}
           </ThemeProvider>
         </body>
