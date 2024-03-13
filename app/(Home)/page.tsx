979d4c6e2557c8c73286ee934b23eb559a33f9a1
Catalin Stroe <alexandru.catalin.stroe@gmail.com>
2024-03-13 02:25:15 +0200

    tina config done

--- app/(Home)/page.tsx
+++ app/(Home)/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 import * as React from "react";
 import { useTheme } from "next-themes";
@@ -7,10 +8,17 @@ import { Spotlight } from "@/components/ui/spotlight";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
+import { useTina } from "tinacms/dist/react";
 
 //TODO: Fix page bottom space. I think is from the footer.
 
-export default function Home() {
+export default function Home(props) {
+  // Pass our data through the "useTina" hook to make it editable
+  const { data } = useTina({
+    query: props.query,
+    variables: props.variables,
+    data: props.data,
+  });
   return (
     <div className="min-h-screen bg-black overflow-hidden">
       <div className=" dark:bg-black bg-white dark:bg-grid-green-500/[0.1] bg-grid-red/[0.2]">
@@ -29,3 +37,5 @@ export default function Home() {
     </div>
   );
 }
+
+//TODO: Implement Tina CMS for editing the page.
